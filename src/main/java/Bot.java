fa4f5e93da33f962f548448779e355ecad630c02
jam643d <jam643d@gmail.com>
2020-04-03 17:41:42 -0400

    Set up general classes for bot actions, set up initial GCal framework.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,4 +1,5 @@
 import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -12,6 +13,39 @@ public class Bot extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(@Nonnull MessageReceivedEvent event){
+
+        //Event specific information
+        User author = event.getAuthor();                //The user that sent the message
+        Message message = event.getMessage();           //The message that was received.
+        MessageChannel channel = event.getChannel();    //This is the MessageChannel that the message was sent to.
+        //  This could be a TextChannel, PrivateChannel, or Group!
+
+        String msg = message.getContentDisplay();              //This returns a human readable version of the Message. Similar to
+        // what you would see in the client.
+
+        if (event.isFromType(ChannelType.TEXT))         //If this message was sent to a Guild TextChannel
+        {
+            //Because we now know that this message was sent in a Guild, we can do guild specific things
+            // Note, if you don't check the ChannelType before using these methods, they might return null due
+            // the message possibly not being from a Guild!
+
+            Guild guild = event.getGuild();             //The Guild that this message was sent in. (note, in the API, Guilds are Servers)
+            TextChannel textChannel = event.getTextChannel(); //The TextChannel that this message was sent to.
+            Member member = event.getMember();          //This Member that sent the message. Contains Guild specific information about the User!
+
+            String name;
+            if (message.isWebhookMessage())
+            {
+                name = author.getName();                //If this is a Webhook message, then there is no Member associated
+            }                                           // with the User, thus we default to the author for name.
+            else
+            {
+                name = member.getEffectiveName();       //This will either use the Member's nickname if they have one,
+            }                                           // otherwise it will default to their username. (User#getName())
+        }
+
+
+
         System.out.println("Received a message:\n\t"+ event.getMessage().getContentDisplay());
     }
 }
---
+++ src/main/java/FFXIV.java
@@ -0,0 +1,7 @@
+public class FFXIV {
+
+    //TODO: Return specified Best In Slot list based on query.
+    public static String BestInSlot(){
+        return "";
+    }
+}
